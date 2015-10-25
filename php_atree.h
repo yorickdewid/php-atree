0498d8c511d91f0c831d2babba4690df455b44fa
yorickdewid <yorick17@outlook.com>
2015-10-25 23:43:41 +0100

    Show version as static function

--- php_atree.h
+++ php_atree.h
@@ -5,10 +5,8 @@
 #include "TSRM.h"
 #endif
 
-#define PHP_ATREE_VERSION "0.9"
-#define PHP_ATREE_EXTNAME "php-atree"
-
-PHP_FUNCTION(atree_init);
+#define PHP_ATREE_VERSION "0.1-dev"
+#define PHP_ATREE_EXTNAME "atree"
 
 extern zend_module_entry atree_module_entry;
 #define phpext_atree_ptr &atree_module_entry
