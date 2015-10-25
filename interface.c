508eb7c8643e87c1bbaf6430ac86dead4ebdcc75
yorickdewid <yorick17@outlook.com>
2015-10-26 00:03:02 +0100

    Only allow classes

---
+++ interface.c
@@ -0,0 +1,105 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "ext/standard/info.h"
+#include "php_atree.h"
+#include "SAPI.h"
+
+#include "atree.h"
+
+/* Structure for Adaptive Tree object. */
+typedef struct _php_atree_db_object  {
+	zend_object zo;
+	int initialised;
+	art_tree *t;
+
+	zend_bool exception;
+
+	zend_llist free_list;
+} php_atree_db_object;
+
+/* Handlers */
+static zend_object_handlers atree_object_handlers;
+
+/* Class entries */
+zend_class_entry *php_atree_sc_entry;
+
+/* {{{ arginfo */
+ZEND_BEGIN_ARG_INFO(arginfo_atree_void, 0)
+ZEND_END_ARG_INFO()
+/* }}} */
+
+/* {{{ proto String Atree::version()
+   Returns theversion as a string constant. */
+PHP_METHOD(atree, version)
+{
+        if (zend_parse_parameters_none() == FAILURE) {
+                return;
+        }
+
+        ZVAL_STRING(return_value, PHP_ATREE_VERSION, 1);
+
+        return;
+}
+/* }}} */
+
+/* {{{ php_atree_class_methods */
+static zend_function_entry php_atree_class_methods[] = {
+	PHP_ME(atree, version, arginfo_atree_void, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+	PHP_FE_END
+};
+/* }}} */
+
+/* {{{ PHP_MINIT_FUNCTION
+*/
+PHP_MINIT_FUNCTION(atree)
+{
+	zend_class_entry ce;
+
+	memcpy(&atree_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+
+	/* Register Atree 3 Class */
+	INIT_CLASS_ENTRY(ce, "Atree", php_atree_class_methods);
+	ce.create_object = php_sqlite3_object_new;
+	atree_object_handlers.clone_obj = NULL;
+	php_atree_sc_entry = zend_register_internal_class(&ce TSRMLS_CC);
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION
+*/
+PHP_MINFO_FUNCTION(atree)
+{
+	php_info_print_table_start();
+	php_info_print_table_header(2, "Adaptive Tree support", "enabled");
+	php_info_print_table_row(2, "Atree module version", PHP_ATREE_VERSION);
+	php_info_print_table_row(2, "Tree algorithm", "ART");
+	php_info_print_table_end();
+
+	DISPLAY_INI_ENTRIES();
+}
+/* }}} */
+
+/* {{{ atree_module_entry
+*/
+zend_module_entry atree_module_entry = {
+    STANDARD_MODULE_HEADER,
+    PHP_ATREE_EXTNAME,
+    NULL,
+    PHP_MINIT(atree),
+    NULL,
+    NULL,
+    NULL,
+    PHP_MINFO(atree),
+    PHP_ATREE_VERSION,
+    STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_ATREE
+ZEND_GET_MODULE(atree)
+#endif
